b67c84f914108e2a0298e0fdd47a49162f78f40e
Artur Mezhidov <artur.mezhidov@itechart-group.com>
2015-07-22 19:17:22 +0300

    Created sorter for "select".

---
+++ JQuery/Scripts/util.js
@@ -0,0 +1,59 @@
+﻿var util = {
+
+	parseArray: function (stringArray) {
+		if (!stringArray) {
+			return undefined;
+		}
+
+		var regExNumber = /(-)?[0-9]+(\.[0-9])?/g; /* Integer and float numbers */
+
+		var regExResult = stringArray.match(regExNumber);
+
+		if (!regExResult) {
+			return undefined;
+		}
+
+		var arr = [];
+		regExResult.forEach(function (item) {
+			arr.push(parseFloat(item));
+		});
+
+		return arr;
+	},
+
+	getValue: function (id) {
+		var control = document.getElementById(id);
+		if (control) {
+			return control.value;
+		}
+		return undefined;
+	},
+
+	setValue: function (id, value) {
+		var control = document.getElementById(id);
+		if (control) {
+			if (control.style.visibility !== "visible") {
+				control.style.visibility = "visible";
+			}
+			control.innerHTML = value;
+		}
+	},
+
+	setError: function (id, message) {
+		var control = document.getElementById(id);
+		if (control) {
+			if (control.style.visibility !== "visible") {
+				control.style.visibility = "visible";
+			}
+			control.innerHTML = "<span class='error'>" + message + "</span>";
+		}
+	},
+
+	isChecked: function (id) {
+		var control = document.getElementById(id);
+		if (control) {
+			return control.checked;
+		}
+		return undefined;
+	}
+};
---
+++ JQuery/Task 3.01/select-sorter.js
@@ -0,0 +1,84 @@
+﻿$(function () {
+
+	var Criterion = {
+		VALUE: 0,
+		TEXT: 1
+	};
+
+	var Direction = {
+		ASC: 0,
+		DESC: 1
+	};
+
+	var DataType = {
+		NUMBER: 0,
+		STRING: 1
+	};
+
+	$.fn.selectSort = function (criterion, direction, dataType) {
+
+		// get elements
+		var selectItems = this.children();
+
+		// determine the direction
+		direction = direction === Direction.DESC
+			? -1
+			: 1;
+
+		// create comparator
+		var comparator = function (firstItem, secondItem) {
+
+			var firstValue;
+			var secondValue;
+
+			if (criterion === Criterion.VALUE) {
+				firstValue = $(firstItem).val();
+				secondValue = $(secondItem).val();
+			} else {
+				firstValue = $(firstItem).text();
+				secondValue = $(secondItem).text();
+			}
+
+			if (dataType === DataType.NUMBER) {
+				firstValue = parseInt(firstValue);
+				secondValue = parseInt(secondValue);
+			} else {
+				firstValue = firstValue.toLowerCase();
+				secondValue = secondValue.toLowerCase();
+			}
+
+			var result = firstValue > secondValue
+				? direction
+				: -direction;
+
+			return result;
+		};
+
+		// sorting
+		var sortedList = $(selectItems).sort(comparator);
+
+		// add new sorted items
+		this.append(sortedList);
+
+		// chaining
+		return this;
+	}
+
+	// Test example
+	$("#bSelectSort").on("click", function () {
+
+		var criterion = util.isChecked("rbSortCriteriaValue")
+			? Criterion.VALUE
+			: Criterion.TEXT;
+
+		var direction = util.isChecked("rbSortDirAsc")
+			? Direction.ASC
+			: Direction.DESC;
+
+		var dataType = util.isChecked("rbSortDataTypeNumber")
+			? DataType.NUMBER
+			: DataType.STRING;
+
+		$("#select").selectSort(criterion, direction, dataType);
+	});
+});
